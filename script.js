fca2cce5a56198be40073c032ad57f4719c61525
m7md-cc <mohamedvip0101@gmail.com>
2026-08-06 07:08:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+
+    if (window.scrollY > 50) {
+        nav.style.background = "rgba(9,9,15,.95)";
+        nav.style.boxShadow = "0 10px 30px rgba(76,201,240,.15)";
+        nav.style.padding = "15px 8%";
+    } else {
+        nav.style.background = "rgba(9,9,15,.75)";
+        nav.style.boxShadow = "none";
+        nav.style.padding = "20px 8%";
+    }
+});
